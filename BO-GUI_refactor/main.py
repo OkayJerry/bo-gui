fb3261956e27fdd1f9538035e05c6ad314c87e7d
Jerry Hoskins <70593138+OkayJerry@users.noreply.github.com>
2023-05-19 16:55:10 -0400

    Delete BO-GUI_refactor directory

--- BO-GUI_refactor/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from components.windows import MainWindow
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    app.exec_()
